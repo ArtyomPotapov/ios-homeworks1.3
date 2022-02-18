cf008a41c2c9e3ef72ad04364bec1fa87533adb8
ArtyomPotapov <artyom.potapov@icloud.com>
2022-02-18 21:58:37 +0300

    simplified TabBar's viewDidLoad()

--- Navigation1.3/SceneDelegate.swift
+++ Navigation1.3/SceneDelegate.swift
@@ -16,12 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        
-        
         let tabBarController = TabBar()
-        
-        
-        
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
         
--- Navigation1.3/TabBar.swift
+++ Navigation1.3/TabBar.swift
@@ -8,40 +8,28 @@
 import UIKit
 
 class TabBar: UITabBarController {
-
+    let feedViewController = FeedViewController()
+    let profileViewController = ProfileViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        UITabBar.appearance().barTintColor = .systemBackground
+        self.tabBar.tintColor = .label
+        setNavigationControllers()
         
-        
-        let feedViewController = FeedViewController()
+    }
+    
+    func setNavigationControllers(){
         let firstNavigationController = UINavigationController(rootViewController: feedViewController)
         firstNavigationController.tabBarItem.title = "FEED"
         firstNavigationController.tabBarItem.image = UIImage(systemName: "pencil.circle")!
         
-        let profileViewController = ProfileViewController()
         let secondNavigationController = UINavigationController(rootViewController: profileViewController)
         secondNavigationController.tabBarItem.title = "PROFILE"
         secondNavigationController.tabBarItem.image = UIImage(systemName: "person")!
-        
+
         self.viewControllers = [firstNavigationController, secondNavigationController]
-        UITabBar.appearance().barTintColor = .systemBackground
-        self.tabBar.tintColor = .label
-        
-        
-        
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
