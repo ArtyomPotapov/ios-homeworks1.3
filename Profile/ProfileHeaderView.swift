a0175de723ec22f56a7cb5c3dcd864a1df7a5f4b
ArtyomPotapov <artyom.potapov@icloud.com>
2022-02-19 02:26:45 +0300

    закончил кнопку

--- Profile/ProfileHeaderView.swift
+++ Profile/ProfileHeaderView.swift
@@ -10,6 +10,7 @@ import UIKit
 class ProfileHeaderView: UIView {
 
     @IBOutlet weak var myPhoto: UIImageView!
+    @IBOutlet weak var showStatusButton: UIButton!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -19,6 +20,17 @@ class ProfileHeaderView: UIView {
         myPhoto.clipsToBounds = true
         myPhoto.layer.borderColor = UIColor.white.cgColor
         myPhoto.layer.borderWidth = 3
+        showStatusButton.layer.cornerRadius = 14
+        showStatusButton.setTitleColor(.white, for: .normal)
+        showStatusButton.setTitleColor(.systemMint, for: .highlighted)
+//
+        showStatusButton.setTitle("Show status", for: .normal)
+        showStatusButton.setTitle("Show status", for: .highlighted)
+
+        showStatusButton.layer.shadowOpacity = 1.0
+        showStatusButton.layer.shadowColor = UIColor.black.cgColor
+        showStatusButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+        showStatusButton.layer.shadowRadius = 4
         
     }
     
