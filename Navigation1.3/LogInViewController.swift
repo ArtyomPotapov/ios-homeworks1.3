5bbe474e693ec2010439df392f1ec49bd69228e9
ArtyomPotapov <artyom.potapov@icloud.com>
2022-03-06 00:19:27 +0300

    сделал stackView и заполнил его текстовыми полями с разделителем

--- Navigation1.3/LogInViewController.swift
+++ Navigation1.3/LogInViewController.swift
@@ -19,12 +19,59 @@ class LogInViewController: UIViewController {
     }()
     
     
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.layer.cornerRadius = 10
+        stackView.layer.borderWidth = 0.5
+        stackView.layer.borderColor = UIColor.lightGray.cgColor
+        stackView.alignment = .fill
+        stackView.distribution = .fillProportionally
+        stackView.backgroundColor = .systemGray6
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var separator: UIView = {
+        let separator = UIView(frame: .zero)
+        separator.backgroundColor = .lightGray
+        separator.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        separator.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+        separator.translatesAutoresizingMaskIntoConstraints = false
+        return separator
+    }()
+    
+    private lazy var emailField: UITextField = {
+        let emailField = UITextField()
+        emailField.backgroundColor = .systemGray6
+        emailField.placeholder = "Email or phone"
+        emailField.textColor = .black
+        emailField.autocapitalizationType = .none
+        emailField.font = .systemFont(ofSize: 16)
+//        emailField.tintColor =
+        emailField.translatesAutoresizingMaskIntoConstraints = false
+        return emailField
+    }()
+    
+    private lazy var passwordField: UITextField = {
+        let passwordField = UITextField()
+        passwordField.placeholder = "Password"
+        passwordField.backgroundColor = .systemGray6
+        passwordField.isSecureTextEntry = true
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        return passwordField
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.isToolbarHidden = true
         view.backgroundColor = .white
         view.addSubview(imageView)
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(emailField)
+        stackView.addArrangedSubview(separator)
+        stackView.addArrangedSubview(passwordField)
         addConstr()
         
             }
@@ -34,7 +81,16 @@ class LogInViewController: UIViewController {
         let imageWidth = imageView.widthAnchor.constraint(equalToConstant: 100)
         let imageCenter = imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         let imageTop = imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120)
-        NSLayoutConstraint.activate([imageHeight, imageWidth, imageCenter, imageTop])
+        let stackHeight = stackView.heightAnchor.constraint(equalToConstant: 100)
+        let stackTop = stackView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 120)
+        let stackLeading = stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        let stackTrailing = stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        let separatorHeight = separator.heightAnchor.constraint(equalToConstant: 0.5)
+        let separatorWidth = separator.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0)
+        let emailLeading = emailField.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 10)
+        
+        
+        NSLayoutConstraint.activate([imageHeight, imageWidth, imageCenter, imageTop, stackHeight, stackTop, stackLeading, stackTrailing, separatorWidth, separatorHeight, emailLeading])
     }
     
     
