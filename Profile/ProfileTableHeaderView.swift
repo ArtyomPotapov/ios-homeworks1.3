b86d11697198325a7292d7fda8d53b5fd2204b0a
ArtyomPotapov <artyom.potapov@icloud.com>
2022-03-07 01:26:57 +0300

    пункт 3, перенёс весь код из ProfileViewController.swift в новый наследник UIView

---
+++ Profile/ProfileTableHeaderView.swift
@@ -0,0 +1,55 @@
+//
+//  ProfileTableHeaderView.swift
+//  Navigation1.3
+//
+//  Created by Artyom Potapov on 07.03.2022.
+//
+
+import UIKit
+
+class ProfileTableHeaderView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        self.addSubview(profileHeaderView)
+        activateConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    var heightViewConstraint: NSLayoutConstraint?
+    
+    private lazy var profileHeaderView: ProfileHeaderView = {
+        let view = ProfileHeaderView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.delegate = self
+        return view
+    }()
+    
+    
+    
+    func activateConstraints(){
+        let topConstraint = profileHeaderView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor)
+        let leadingConstraint = profileHeaderView.leadingAnchor.constraint(equalTo: self.leadingAnchor)
+        let trailConstraint = profileHeaderView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        heightViewConstraint =  profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+
+        NSLayoutConstraint.activate([topConstraint, leadingConstraint, trailConstraint, heightViewConstraint].compactMap({$0}))
+    }
+}
+
+extension ProfileTableHeaderView: ViewExpandable {
+    func expandedView(isExpand: Bool) {
+        heightViewConstraint?.constant = isExpand ? 500 : 220
+        UIView.animate(withDuration: 0.2) {
+            self.layoutIfNeeded()
+        }
+    }
+
+}
--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -22,54 +22,9 @@ struct PostModel {
 
 class ProfileViewController: UIViewController {
     
-        
-    private lazy var newButton: UIButton = {
-        var button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("New button from HW2.2", for: .normal)
-        button.backgroundColor = .systemRed
-        return button
-    }()
-    
-    var heightViewConstraint: NSLayoutConstraint?
-    
-    private lazy var profileHeaderView: ProfileHeaderView = {
-        let view = ProfileHeaderView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.delegate = self
-        return view
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
         title = "Профиль"
-        view.addSubview(profileHeaderView)
-        view.addSubview(newButton)
-        activateConstraints()
-        
     }
-    
-    func activateConstraints(){
-        let topConstraint = profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
-        let leadingConstraint = profileHeaderView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor)
-        let trailConstraint = profileHeaderView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
-        heightViewConstraint =  profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
-        let newButtonLeading = newButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor)
-        let newButtomTrailing = newButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
-        let newButtomBottom = newButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
-        
-        NSLayoutConstraint.activate([topConstraint, leadingConstraint, trailConstraint, heightViewConstraint, newButtomBottom, newButtonLeading, newButtomTrailing].compactMap({$0}))
-    }
-}
-
-extension ProfileViewController: ViewExpandable {
-    func expandedView(isExpand: Bool) {
-        heightViewConstraint?.constant = isExpand ? 500 : 220
-        UIView.animate(withDuration: 0.2) {
-            self.view.layoutIfNeeded()
-            print(isExpand)
-            print(self.heightViewConstraint!)
-        }
-    }
+   
 }
