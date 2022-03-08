dcdc9b8cd991a4e87d267bae032e036cb4d29e2b
ArtyomPotapov <artyom.potapov@icloud.com>
2022-03-09 01:55:54 +0300

    создал все view в ячейче cell

--- Profile/PostTableViewCell.swift
+++ Profile/PostTableViewCell.swift
@@ -9,6 +9,54 @@ import UIKit
 
 class PostTableViewCell: UITableViewCell {
 
+    private lazy var bottomStackView: UIStackView = {
+       let buttomStackView = UIStackView()
+        buttomStackView.axis = .horizontal
+        buttomStackView.spacing = 10
+        buttomStackView.translatesAutoresizingMaskIntoConstraints = false
+       return buttomStackView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+       return stackView
+    }()
+    
+    private lazy var authorLabel: UILabel = {
+        let authorLabel = UILabel()
+        authorLabel.font = UIFont(name: "Georgia", size: 18)
+        authorLabel.translatesAutoresizingMaskIntoConstraints = false
+        return authorLabel
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.textColor = .systemGray3
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        return descriptionLabel
+    }()
+    
+    private lazy var viewsLabel: UILabel = {
+        let viewsLabel = UILabel()
+        viewsLabel.translatesAutoresizingMaskIntoConstraints = false
+        return viewsLabel
+    }()
+    
+    private lazy var likesLabel: UILabel = {
+        let likesLabel = UILabel()
+        likesLabel.translatesAutoresizingMaskIntoConstraints = false
+        return likesLabel
+    }()
+    
+    private lazy var myImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     
     
     override func awakeFromNib() {
@@ -21,5 +69,27 @@ class PostTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(stackView)
+        stackView.addArrangedSubview(authorLabel)
+        stackView.addArrangedSubview(myImageView)
+        stackView.addArrangedSubview(descriptionLabel)
+        stackView.addArrangedSubview(bottomStackView)
+        bottomStackView.addArrangedSubview(viewsLabel)
+        bottomStackView.addArrangedSubview(likesLabel)
+        addMyConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addMyConstraints(){
+        
+        
+    }
+    
 }
