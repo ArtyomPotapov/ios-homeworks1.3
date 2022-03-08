9836bb86afb26870a774dd8cbee939cf41b622ba
ArtyomPotapov <artyom.potapov@icloud.com>
2022-03-08 23:26:36 +0300

    добавил tableView

---
+++ Profile/PostTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostTableViewCell.swift
+//  Navigation1.3
+//
+//  Created by Artyom Potapov on 07.03.2022.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -21,10 +21,56 @@ struct PostModel {
 
 
 class ProfileViewController: UIViewController {
+   
+    
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 44
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DefaultCell")
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "PostCell")
+        tableView.backgroundColor = .black
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Профиль"
+        view.addSubview(tableView)
+        addConstraint()
     }
    
+    func addConstraint(){
+        let tableTop = tableView.topAnchor.constraint(equalTo: view.topAnchor)
+        let tableBottom = tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        let tableLeft = tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let tableTrailing = tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        
+        NSLayoutConstraint.activate([tableTop, tableLeft, tableBottom, tableTrailing])
+    }
+    
+}
+extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as? PostTableViewCell  else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell", for: indexPath)
+            return cell
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = ProfileTableHeaderView()
+        return header
+        
+    }
+    
 }
