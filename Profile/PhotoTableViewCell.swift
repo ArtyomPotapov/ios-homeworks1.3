6fd196e90cb4be828fb4857b2ee6b99b80169f08
ArtyomPotapov <artyom.potapov@icloud.com>
2022-03-18 02:20:34 +0300

    закончил макет новой ячейки таблицы

--- Profile/PhotoTableViewCell.swift
+++ Profile/PhotoTableViewCell.swift
@@ -9,10 +9,24 @@ import UIKit
 
 class PhotoTableViewCell: UITableViewCell {
 
-    private lazy var myStackView: UIStackView = {
+    private lazy var verticalStackView: UIStackView = {
        let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.spacing = 10
+        stackView.spacing = 12
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+       return stackView
+    }()
+    private lazy var horizontalTopStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+       return stackView
+    }()
+    
+    private lazy var horizontalStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
         stackView.translatesAutoresizingMaskIntoConstraints = false
        return stackView
     }()
@@ -20,10 +34,28 @@ class PhotoTableViewCell: UITableViewCell {
     private lazy var photosLabel: UILabel = {
         let photosLabel = UILabel()
         photosLabel.text = "Photos"
+        photosLabel.textAlignment = .left
         photosLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 24)
+        photosLabel.setContentHuggingPriority(UILayoutPriority(300), for: .horizontal)
         photosLabel.translatesAutoresizingMaskIntoConstraints = false
         return photosLabel
     }()
+    
+//    private lazy var arrowLabel: UILabel = {
+//        let photosLabel = UILabel()
+//        photosLabel.imageView = UIImage(systemName: "pencil.circle")!
+//        photosLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 24)
+//        photosLabel.translatesAutoresizingMaskIntoConstraints = false
+//        return photosLabel
+//    }()
+//
+    private lazy var arrowImageView1: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: photosLabel.layer.frame.height))
+        imageView.image = UIImage(systemName: "arrowshape.turn.up.right")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     
     private lazy var myImageView1: UIImageView = {
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: (self.contentView.frame.width / 4 - 48), height: 60))
@@ -80,7 +112,25 @@ class PhotoTableViewCell: UITableViewCell {
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-       
+        contentView.addSubview(verticalStackView)
+        verticalStackView.addArrangedSubview(horizontalTopStackView)
+        verticalStackView.addArrangedSubview(horizontalStackView)
+        horizontalTopStackView.addArrangedSubview(photosLabel)
+        horizontalTopStackView.addArrangedSubview(arrowImageView1)
+        horizontalStackView.addArrangedSubview(myImageView1)
+        horizontalStackView.addArrangedSubview(myImageView2)
+        horizontalStackView.addArrangedSubview(myImageView3)
+        horizontalStackView.addArrangedSubview(myImageView4)
+        setConstraints()
+    }
+    
+    func setConstraints(){
+        let topStackView = verticalStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12)
+        let bottomStackView = verticalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12)
+        let leadStackView = verticalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12)
+        let trailStackView = verticalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12)
+        
+        NSLayoutConstraint.activate([topStackView, bottomStackView, trailStackView, leadStackView])
     }
     
     required init?(coder: NSCoder) {
