855c113e68056c0b7752767bdda49ec89125839f
ArtyomPotapov <artyom.potapov@icloud.com>
2022-03-24 22:23:12 +0300

    появился код про UIGestureRecognizer

--- Profile/GestureViewController.swift
+++ Profile/GestureViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 class GestureViewController: UIViewController {
     
     var widthViewConstraint: NSLayoutConstraint?
+    var heightViewConstraint: NSLayoutConstraint?
+    var topConstraint: NSLayoutConstraint?
+    var leadingConstraint: NSLayoutConstraint?
+    var isExpanded = false
+    
+    let tapGestureRecognizer = UITapGestureRecognizer()
+    
     
     private lazy var myImageView: UIImageView = {
         var myImageView = UIImageView()
@@ -25,14 +32,15 @@ class GestureViewController: UIViewController {
         view.addSubview(myImageView)
         activateConstraints()
         setImage()
+        setGesture()
     }
     
     func activateConstraints(){
-        let topConstraint = myImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
-        let leadingConstraint = myImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
-//        let trailConstraint = myImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        let heightViewConstraint =  myImageView.heightAnchor.constraint(equalToConstant: 120)
+        topConstraint = myImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
+        leadingConstraint = myImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        heightViewConstraint =  myImageView.heightAnchor.constraint(equalToConstant: 120)
         widthViewConstraint =  myImageView.widthAnchor.constraint(equalToConstant: 120)
+        
         NSLayoutConstraint.activate([topConstraint, leadingConstraint, heightViewConstraint, widthViewConstraint].compactMap({$0}))
     }
 
@@ -41,9 +49,19 @@ class GestureViewController: UIViewController {
         myImageView.contentMode = .scaleAspectFill
         myImageView.layer.cornerRadius = 60
         myImageView.clipsToBounds = true
-        myImageView.layer.borderColor = UIColor.white.cgColor
+        myImageView.layer.borderColor = UIColor.systemRed.cgColor
         myImageView.layer.borderWidth = 3
     }
   
+    
+    func setGesture(){
+        tapGestureRecognizer.addTarget(self, action: #selector(tapView(_:)))
+        myImageView.addGestureRecognizer(tapGestureRecognizer)
+    }
 
+    @objc func tapView(_ tapGR:UITapGestureRecognizer){
+        guard tapGestureRecognizer === tapGR else {return}
+        
+        
+    }
 }
